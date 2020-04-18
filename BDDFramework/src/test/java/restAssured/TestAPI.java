573cbdfe9bef3ab561a17ea2c1e573c6a4c9084a
Siva Shankar <durgaj@issquaredinc.com>
2020-04-18 13:30:17 +0530

    Adding restassured

---
+++ BDDFramework/src/test/java/restAssured/TestAPI.java
@@ -0,0 +1,35 @@
+package restAssured;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class TestAPI {
+	
+	@Test
+	public void GetWeatherDetails()
+	{   
+		// Specify the base URL to the RESTful web service
+		RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+
+		// Get the RequestSpecification of the request that you want to sent
+		// to the server. The server is specified by the BaseURI that we have
+		// specified in the above step.
+		RequestSpecification httpRequest = RestAssured.given();
+
+		// Make a request to the server by specifying the method Type and the method URL.
+		// This will return the Response from the server. Store the response in a variable.
+		Response response = httpRequest.request(Method.GET, "/Chennai");
+
+		// Now let us print the body of the message to see what response
+		// we have recieved from the server
+		String responseBody = response.getBody().asString();
+		System.out.println("Response Body is =>  " + responseBody);
+
+	}
+
+
+}
