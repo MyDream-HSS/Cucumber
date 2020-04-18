49e8255d753a4cec57cb9016ebe85d902b560cbf
Siva Pavan <nbupalem@issquaredinc.com>
2020-04-18 21:15:14 +0530

    Rest assured updated

--- BDDFramework/src/test/java/restAssured/TestAPI.java
+++ BDDFramework/src/test/java/restAssured/TestAPI.java
@@ -6,30 +6,36 @@ import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import junit.framework.Assert;
 
 public class TestAPI {
 	
 	@Test
 	public void GetWeatherDetails()
 	{   
-		// Specify the base URL to the RESTful web service
 		RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
-
-		// Get the RequestSpecification of the request that you want to sent
-		// to the server. The server is specified by the BaseURI that we have
-		// specified in the above step.
 		RequestSpecification httpRequest = RestAssured.given();
 
-		// Make a request to the server by specifying the method Type and the method URL.
-		// This will return the Response from the server. Store the response in a variable.
 		Response response = httpRequest.request(Method.GET, "/Chennai");
-
-		// Now let us print the body of the message to see what response
-		// we have recieved from the server
+		
 		String responseBody = response.getBody().asString();
 		System.out.println("Response Body is =>  " + responseBody);
-
+		Assert.assertTrue(response.getBody().asString().contains("Chennai"));
+		
+		int Response = response.statusCode();
+		System.out.println("Response Code is =>  " + Response);
 	}
-
+	
+	@Test
+	public void getdetails() {
+		RestAssured.baseURI =	"https://www.toolsqa.com/rest-assured";
+		RequestSpecification httpRequest = RestAssured.given();
+		
+//		Response response = httpRequest.request(Method.GET, "/post-request-using-rest-assured");
+		Response response = httpRequest.get();
+		String responseBody = response.getBody().asString();
+		System.out.println("Response Body is =>  " + responseBody);
+		
+	}
 
 }
